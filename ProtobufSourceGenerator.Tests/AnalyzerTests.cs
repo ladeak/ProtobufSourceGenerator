210043c6bb9eb70e7e2ece35a1091eedb61049f7
Laszlo Deak <ladeak87@windowslive.com>
2023-01-12 22:34:47 +0100

    Adding analyzer cases to handle non-partial nested types and to add info non-generating properties

--- ProtobufSourceGenerator.Tests/AnalyzerTests.cs
+++ ProtobufSourceGenerator.Tests/AnalyzerTests.cs
@@ -44,4 +44,129 @@ public partial class Entity
         var test = new AnalyzeCS() { TestCode = code };
         await test.RunAsync();
     }
+
+    [Fact]
+    public async Task ProtoMember_NotPartial_MissingProtoContract_NoError()
+    {
+        string code = @"namespace Test;
+public class SomeEntity
+{
+    public int Value { get; set; }
+}
+
+[ProtoBuf.ProtoContract]
+public partial class Entity
+{   
+    [ProtoBuf.ProtoMember(1)]
+    public SomeEntity Id { get; set; }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ProtoIgnore_NotPartial_MissingProtoContract_NoError()
+    {
+        string code = @"namespace Test;
+public class SomeEntity
+{
+    public int Value { get; set; }
+}
+
+[ProtoBuf.ProtoContract]
+public partial class Entity
+{   
+    [ProtoBuf.ProtoMember(1)]
+    public SomeEntity Id { get; set; }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task Info_ForNonGeneratingProperty()
+    {
+        string code = @"namespace Test;
+[ProtoBuf.ProtoContract]
+public partial class Entity
+{   
+    public int Id { get; init; }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        DiagnosticResult expected = VerifyCS.Diagnostic("Proto03").WithLocation(5, 16).WithArguments(string.Empty);
+        test.ExpectedDiagnostics.Add(expected);
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task InfoIgnore_ForNonGeneratingProperty()
+    {
+        string code = @"namespace Test;
+[ProtoBuf.ProtoContract]
+public partial class Entity
+{   
+    [ProtoBuf.ProtoIgnore]
+    public int Id { get; init; }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ParentPartialType_NoDiagnosticError()
+    {
+        string code = @"namespace Test;
+public partial class ParentEntity
+{
+    public int Value { get; set; }
+
+    [ProtoBuf.ProtoContract]
+    public partial class Entity
+    {   
+        public string Id { get; set; }
+    }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ParentType_NotPartial_DiagnosticError()
+    {
+        string code = @"namespace Test;
+public class ParentEntity
+{
+    public int Value { get; set; }
+
+    [ProtoBuf.ProtoContract]
+    public partial class Entity
+    {   
+        public string Id { get; set; }
+    }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        DiagnosticResult expected = VerifyCS.Diagnostic("Proto02").WithLocation(7, 26).WithArguments(string.Empty);
+        test.ExpectedDiagnostics.Add(expected);
+        await test.RunAsync();
+    }
+
+    [Fact]
+    public async Task ParentStructType_NotPartial_DiagnosticError()
+    {
+        string code = @"namespace Test;
+public struct ParentEntity
+{
+    public int Value { get; set; }
+
+    [ProtoBuf.ProtoContract]
+    public partial class Entity
+    {   
+        public string Id { get; set; }
+    }
+}";
+        var test = new AnalyzeCS() { TestCode = code };
+        DiagnosticResult expected = VerifyCS.Diagnostic("Proto02").WithLocation(7, 26).WithArguments(string.Empty);
+        test.ExpectedDiagnostics.Add(expected);
+        await test.RunAsync();
+    }
 }
--- ProtobufSourceGenerator/Analyzer.cs
+++ ProtobufSourceGenerator/Analyzer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -12,14 +13,34 @@ namespace ProtobufSourceGenerator
     {
         private const string Category = "ProtoSourceGeneration";
         private static DiagnosticDescriptor Rule01 = new DiagnosticDescriptor("Proto01", "Type must be partial with ProtoContract attribute", "Type must be partial with ProtoContract attribute", Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: "Type must be partial with ProtoContract attribute.");
+        private static DiagnosticDescriptor Rule02 = new DiagnosticDescriptor("Proto02", "Nested type's parent must be partial type", "Nested type's parent must be partial type", Category, DiagnosticSeverity.Error, isEnabledByDefault: true, description: "Nested type's parent must be partial type.");
+        private static DiagnosticDescriptor Rule03 = new DiagnosticDescriptor("Proto03", "Consider attributing property with ProtoMember", "Consider attributing property with ProtoMember", Category, DiagnosticSeverity.Info, isEnabledByDefault: true, description: "This property is not considered for ProtoBuf source generation. Consider manually marking the type with ProtoIgnore or ProtoMember attributes.");
 
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule01); } }
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule01, Rule02, Rule03); } }
 
         public override void Initialize(AnalysisContext context)
         {
             context.EnableConcurrentExecution();
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
             context.RegisterSymbolAction(AnalyzeProperty, SymbolKind.Property);
+            context.RegisterSymbolAction(AnalyzeTypeHierarchy, SymbolKind.NamedType);
+        }
+
+        private void AnalyzeTypeHierarchy(SymbolAnalysisContext context)
+        {
+            if (context.Symbol is not INamedTypeSymbol typeSymbol)
+                return;
+
+            typeSymbol = typeSymbol.ContainingType;
+            while (typeSymbol != null)
+            {
+                if (!IsPartial(typeSymbol))
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule02, context.Symbol.Locations.First(), string.Empty));
+                    return;
+                }
+                typeSymbol = typeSymbol.ContainingType;
+            }
         }
 
         private void AnalyzeProperty(SymbolAnalysisContext context)
@@ -27,18 +48,29 @@ namespace ProtobufSourceGenerator
             if (context.Symbol is not IPropertySymbol propertySymbol)
                 return;
 
+            if (!PropertyAttributeParser.CanGenerateProperty(propertySymbol))
+            {
+                if (!PropertyAttributeParser.HasProtoProperties(propertySymbol, out _))
+                    context.ReportDiagnostic(Diagnostic.Create(Rule03, context.Symbol.Locations.First(), string.Empty));
+                return;
+            }
+
             if (propertySymbol.Type is not INamedTypeSymbol namedType || namedType.SpecialType != SpecialType.None)
                 return;
 
             bool hasProtoContract = namedType.GetAttributes().Any(x => x.AttributeConstructor.ToString() != "ProtoBuf.ProtoContractAttribute");
-
-            bool isPartial = namedType.DeclaringSyntaxReferences.First().GetSyntax() is TypeDeclarationSyntax typeDeclaration && typeDeclaration.Modifiers.Any(x => x.IsKeyword() && x.IsKind(SyntaxKind.PartialKeyword));
+            bool isPartial = IsPartial(namedType);
 
             if (hasProtoContract && isPartial)
                 return;
 
             var diagnostic = Diagnostic.Create(Rule01, context.Symbol.Locations.First(), string.Empty);
             context.ReportDiagnostic(diagnostic);
+        }
+
+        private bool IsPartial(INamedTypeSymbol namedType)
+        {
+            return namedType.DeclaringSyntaxReferences.First().GetSyntax() is TypeDeclarationSyntax typeDeclaration && typeDeclaration.Modifiers.Any(x => x.IsKeyword() && x.IsKind(SyntaxKind.PartialKeyword));
         }
     }
 }
